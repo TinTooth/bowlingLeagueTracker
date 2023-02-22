f27adb8777ad37491a8ecbbef75f6cd989aadd88
Ben O'Connell <boconne4@gmail.com>
2023-02-22 10:40:16 -0600

    added TeamTable using material UI Table

--- bowling/src/App.js
+++ bowling/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TeamTable from './Components/TeamTable/TeamTable';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <TeamTable></TeamTable>
     </div>
   );
 }
---
+++ bowling/src/Components/TeamTable/TeamTable.jsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+import * as teams from "../../data.json";
+
+const TeamTable = () => {
+
+    let data = teams;
+
+    return ( 
+        <>
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Player</TableCell>
+              <TableCell align="left">Average</TableCell>
+              <TableCell align="right">Handicap</TableCell>
+              <TableCell align="center">Games</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.teams.players.map((player,i) => (
+              <TableRow
+              key={i}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="left">{player.name}</TableCell>
+                <TableCell align="left">{player.average}</TableCell>
+                <TableCell align="right">{player.handicap}</TableCell>
+                <TableCell align="center">{player.games}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+            </>
+     );
+}
+ 
+export default TeamTable;
